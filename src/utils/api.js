853e999efdc2520ce9ea33426ee19971d544b193
khalidYousrygit <k@gmail.com>
2023-12-08 02:11:30 +0200

    nothing important

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "https://api.themoviedb.org/3";
-const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
+const VITE_APP_TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-  Authorization: "bearer " + TMDB_TOKEN,
+  Authorization: "bearer " + VITE_APP_TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
